44e532dd41974ae6fab4f7ba01f512cdf9614801
olivrap <olivrap@gmail.com>
2022-06-16 01:36:47 -0300

    Selecting location

--- MutantOp.py
+++ MutantOp.py
@@ -1,14 +1,20 @@
 import tkinter as tk
 from tkinter import filedialog
+import sys
 
 root = tk.Tk()
 root.withdraw()
 
 file_path = filedialog.askopenfilename()
-
-
 f = open(file_path, "r")
-code  = print(f.read())
+
+code  = f.read()
+codeAux = code.replace("Rafa", "okay", 1) 
+
+print(codeAux)
+
+
+#
 
 
 
