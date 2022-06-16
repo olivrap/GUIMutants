d7454ec62cc6a3ea55673b12051a13bfca2cb972
olivrap <olivrap@gmail.com>
2022-06-16 19:49:12 -0300

    SWI - implemented and tested - Good to go.

--- MutantOp.py
+++ MutantOp.py
@@ -25,6 +25,10 @@ def replacer(s, newstring, index, nofail=False):
     # insert the new string between "slices" of the original
     return s[:index] + newstring + s[index + 1:]
 
+# replaces the last occurence of some string by other
+def rreplace(string, stringOut, stringIn):
+    return (string[::-1].replace(stringOut[::-1], stringIn[::-1], 1))[::-1]
+
 def saveMO(filePath, MOName, MOCode, count):
     filename = os.path.basename(filePath) 
     from pathlib import Path
@@ -59,21 +63,68 @@ def rew(writtenCode): #mutant generation for REW mutant operators
             saveMO(file_path, "rew", newString, count)
             count +=1;
             initialPos = 1+writtenCode.find(".add(", initialPos, finalPos)
-        print(count," GUI-based mutants generated from REW") 
+        print(count-1," GUI-based mutants generated from REW") 
     else:
         print("No mutants were created from REW")
-     
+
+
+def swiEnable(writtenCode): #mutant generation for REW mutant operators    
+    if writtenCode.find("setEnabled(false);"):
+        ##print("O num de ocorrencias eh ", writtenCode.count("setEnabled(false)"))
+        count = 1;
+        initialPos = writtenCode.find("setEnabled(false);")
+        finalPos = len(writtenCode)
+
+        while (-1 != writtenCode.find("setEnabled(false);", initialPos, finalPos)):
+            pos = writtenCode.find("setEnabled(false);", initialPos, finalPos)
+            piece01 = writtenCode[0:pos]
+            piece02 = writtenCode[pos:finalPos]
+            piece03 = piece02.replace("setEnabled(false);", "setEnabled(true);", 1)
+
+            newString = piece01+piece03
+            saveMO(file_path, "swi", newString, count)
+            count +=1;
+            initialPos = 1+writtenCode.find("setEnabled(false);", initialPos, finalPos)
+        print(count-1," GUI-based mutants generated from SWI - Enabled") 
+    else:
+        print("No mutants were created from SWI")
+
+def swiVisible(writtenCode): #mutant generation for REW mutant operators    
+    if writtenCode.find("setVisible(true);"):
+        ##print("O num de ocorrencias eh ", writtenCode.count("setEnabled(false)"))
+        count = 1;
+        initialPos = writtenCode.find("setVisible(true);")
+        finalPos = len(writtenCode)
+
+        while (-1 != writtenCode.find("setVisible(true);", initialPos, finalPos)):
+            pos = writtenCode.find("setVisible(true);", initialPos, finalPos)
+            piece01 = writtenCode[0:pos]
+            piece02 = writtenCode[pos:finalPos]
+            piece03 = piece02.replace("setVisible(true);", "setVisible(false);", 1)
+
+            newString = piece01+piece03
+            saveMO(file_path, "swi", newString, count)
+            count +=1;
+            initialPos = 1+writtenCode.find("setVisible(true);", initialPos, finalPos)
+        print(count-1," GUI-based mutants generated from SWI - Visible") 
+    else:
+        print("No mutants were created from SWI")
+
 
 def numbers_to_strings(op): #switch case based on user's option
     match op:
         case "all":
             rew(code)
-            print("-- all of the Mutant operators are being applied --")
+            swiEnable(code)
+            swiVisible(code)
+            print("-- all of the Mutant operators were applied --")
             return "implement all of the mutant operators"
         case "rew": #GUI mutant operators 01
             rew(code)
             return "MO - Remove Existing Widget"
         case "swi": #GUI mutant operators 02
+            swiEnable(code)
+            swiVisible(code)
             return  "SWI - Set Widget Invisible"   
         case _:
             return "unknow command - please, do it again" 
