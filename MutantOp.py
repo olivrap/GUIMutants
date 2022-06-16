9b7637ff2da4cd8ddd935d17f35b564aafed8136
olivrap <olivrap@gmail.com>
2022-06-16 16:41:54 -0300

    REW - code cleaned

--- MutantOp.py
+++ MutantOp.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import os #file manipulation
+import os
 import pathlib
 from tkinter import filedialog
 import sys
@@ -9,11 +9,7 @@ root.withdraw()
 
 file_path = filedialog.askopenfilename()
 f = open(file_path, "r")
-
 code  = f.read()
-codeAux = code.replace("Rafa", "okay", 1) 
-
-print(codeAux)
 
 def replacer(s, newstring, index, nofail=False):
     # raise an error if index is outside of the string
@@ -29,7 +25,6 @@ def replacer(s, newstring, index, nofail=False):
     # insert the new string between "slices" of the original
     return s[:index] + newstring + s[index + 1:]
 
-
 def saveMO(filePath, MOName, MOCode, count):
     filename = os.path.basename(filePath) 
     from pathlib import Path
@@ -37,94 +32,57 @@ def saveMO(filePath, MOName, MOCode, count):
     root = path.parent.absolute()
     dir = MOName
     pathRew = os.path.join(root, dir)
+    if (pathRew)
     try:
         os.mkdir(pathRew)
     except FileExistsError:
+        # directory is already created
         print("Directory " , pathRew ,  " already exists")
-
     try: 
         fileMO = pathRew+"/"+str(count)+filename
         mo_File = open(fileMO, "w")
         mo_File.write(MOCode)
     except Exception:
+        # file generation failed
         print("Something went wrong -- refresh your folder")
 
-
-
-    ## parent = f.parent.absolute()
-    ## print(parent)
-    ##if not os.filePath.exists(filePath.parent)
-
-
-
-def rew(writtenCode):
-    
+def rew(writtenCode): #mutant generation for REW mutant operators    
     if writtenCode.find("add("):
         count = 1;
         initialPos = writtenCode.find("add(")-1
         finalPos = len(writtenCode)
-
         while (-1 != writtenCode.find(".add(", initialPos, finalPos)):
-            ## print("Achei", writtenCode.find(".add", initialPos, finalPos))
             pos = writtenCode.find(".add(", initialPos, finalPos)
             lastLine = writtenCode.rfind('\n', 0, pos)
             newString = replacer(writtenCode, "\n\t//", lastLine)
             saveMO(file_path, "rew", newString, count)
             count +=1;
             initialPos = 1+writtenCode.find(".add(", initialPos, finalPos)
-                  
-       # initialPos = writtenCode.rfind('\n', 0, pos)
+        print(count," GUI-based mutants generated from REW") 
     else:
-        print("No mutants created to REW")
+        print("No mutants were created from REW")
+     
 
-"""
-    while writtenCode.rfind('\n', 0, pos):
-        pos = writtenCode.find("add")
-        num += 1
-        print("Ocorrencia ", num)
-        initialPos = pos
-        
-
-    if writtenCode.find("add"):
-        print("yes, we can")
-        pos = writtenCode.find("add")
-        lastLine = writtenCode.rfind('\n', 0, pos)
-        print("Position ", pos)
-        print("Last bn", lastLine)
-        newString = replacer(writtenCode, "\n\t//", lastLine)
-        print("---------------\n\n\n")
-        print(newString)
-        print("---------------\n\n\n")
-        saveMO(file_path, "rew", newString)
-        print("identificar widgets e removê-los\n criar uma pasta (se já existir, sobrescrever)\n salvar os códigos mutados\n fechar a pasta")
-    #identificar widgets e removê-los
-    #criar uma pasta (se já existir, sobrescrever)
-    #salvar os códigos mutados
-    #fechar a pasta
-"""
-
-def numbers_to_strings(op):
+def numbers_to_strings(op): #switch case based on user's option
     match op:
         case "all":
             print("-- all of the Mutant operators are being applied --")
             return "implement all of the mutant operators"
-        case "rew":
-            rew(codeAux)
+        case "rew": #GUI mutant operators 01
+            rew(code)
             return "MO - Remove Existing Widget"
-        case "swi":
+        case "swi": #GUI mutant operators 02
             return  "SWI - Set Widget Invisible"   
         case _:
-            return "unknow command" 
-
-op = sys.argv[1]
-print(numbers_to_strings(op))  
+            return "unknow command - please, do it again" 
 
 
+def main():
+    ### reading user's option on how to create the GUI-based mutants
+    op = sys.argv[1]
+    print(numbers_to_strings(op))  
 
-#cases
-#
-#
-#
-#
-#
-##
+
+##calling our main
+if __name__== "__main__":
+   main()
