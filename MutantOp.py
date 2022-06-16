8eb00079af8a71cf8633d5445cb42ccb417711b9
olivrap <olivrap@gmail.com>
2022-06-16 09:43:19 -0300

    Creating switch-case structure

--- MutantOp.py
+++ MutantOp.py
@@ -13,15 +13,26 @@ codeAux = code.replace("Rafa", "okay", 1)
 
 print(codeAux)
 
-#op = argv[1]
+def numbers_to_strings(op):
+    match op:
+        case "all":
+            print("-- all of the Mutant operators are being applied --")
+            return "implement all of the mutant operators"
+        case "rew":
+            return "MO - Remove Existing Widget"
+        case "swi":
+            return  "SWI - Set Widget Invisible"   
+        case _:
+            return "unknow command" 
 
+op = sys.argv[1]
 
-print(sys.argv[1])
+print(numbers_to_strings(op))    
 
-
-print ("Number of arguments:", len(sys.argv))
-print ("(Argument List: " , str(sys.argv))
-
-
-
-
+#cases
+#
+#
+#
+#
+#
+##
